cc81e6dfdac9ab89ded8d9bde9d58ccc99ff9c80
Vlad Ocneanu <ocniocni@yahoo.ro>
2023-04-26 11:08:20 +0300

    Changed API URL

--- src/Url.tsx
+++ src/Url.tsx
@@ -1,2 +1,1 @@
-//export const backEndUrl: string = "https://showcase-api-production.up.railway.app/";
-export const backEndUrl: string = "http://localhost:3000/";
+export const backEndUrl: string = "https://showcase-api-production.up.railway.app/";
